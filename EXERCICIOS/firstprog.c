284fa95b2d1770b6743197576a535cf8fef27318
Cristiano Marques Silva <cristimarsilva.cris@gmail.com>
2022-11-27 17:16:28 -0300

    create firstprog.c
     Esse programa ira repitir 10 vezes a frase "Hello world!"

---
+++ EXERCICIOS/firstprog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// esse programa ira repitir 10 vezes a frase "Hello world!"
+int main (){
+    int i;
+    for(i=0; i<10;i++){
+        puts("Hello world!\n");
+    }
+    return 0;
+}
